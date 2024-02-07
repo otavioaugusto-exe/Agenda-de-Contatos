591eee43445f7922f603911155b539d12a2b0b7e
Otávio Augusto <58156476+otavioaugusto-exe@users.noreply.github.com>
2024-02-07 15:17:57 -0300

    fix: better submit criteria

--- main.js
+++ main.js
@@ -4,37 +4,25 @@ const btnFechar = document.getElementById("btn-fechar")
 const formMenu = document.getElementById("form-menu")
 const nomeContato = document.getElementById(`nome-contato`)
 const telefoneContato = document.getElementById(`telefone-contato`)
-const tableContatos = document.getElementById(`table-contatos`)
+const tableContatos = document.getElementById(`table-body`)
 const nomesArray = []
 const telefoneArray = []
 let linhas = ''
 
-
+console.log(tableContatos)
 function adicionarLinhas() {
     let linha = `<tr>`
     linha += `<td>${nomeContato.value}</td>`
     linha += `<td>${telefoneContato.value}</td>`
     linha += `</tr>`
     linhas += linha
-    tableContatos.innerHTML= linhas
+    tableContatos.innerHTML = linhas
 }
 
 function pushInfo() {
     telefoneArray.push(telefoneContato.value)
     nomesArray.push(nomeContato.value)
 }
-
-form.addEventListener('submit',function(e) {
-    e.preventDefault();
-    if(telefoneArray.includes(telefoneContato.value)) {
-        alert("Este contato já foi resgistrado!")
-    } else {
-        pushInfo()
-        adicionarLinhas()
-        nomeContato.value = ""
-        telefoneContato.value = ""
-    }
-})
 
 btnFechar.addEventListener('click', function(e) {
     formMenu.style.display = 'none'
@@ -52,6 +40,22 @@ telefoneContato.addEventListener('keyup', function(e) {
     telefoneContato.value = ('(' + num.substring(0,2) + ') ' + num.substring(2,7) + '-' + num.substring(7,11)); 
 }
 )
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    if (telefoneContato.value === "() -" || nomeContato.value === "" || telefoneContato.value === "") {
+        alert("Preencha os campos!")} else {
+            if(telefoneArray.includes(telefoneContato.value)) {
+                alert("Este contato já foi resgistrado!")
+            } else {
+                pushInfo()
+                adicionarLinhas()
+                nomeContato.value = ""
+                telefoneContato.value = ""
+            }
+        }
+})
 
 
 
